6e9cd9e062f85ca2191168907f2b97bb3c0fafa6
mewtwo-chi <ffchixiang@gmail.com>
2019-10-30 23:26:47 +0800

    选择器

--- PickerViewSample/PickerViewSample/ViewController.m
+++ PickerViewSample/PickerViewSample/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIPickerViewDelegate, UIPickerViewDataSource>
 
 @end
 
@@ -25,6 +25,36 @@
     self.pickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0, 0, pickerViewWidth, pickerViewHeight)];
     
     [self.view addSubview:self.pickerView];
+    
+    //添加标签
+    CGFloat labelWidth = 200;
+    CGFloat labelHeight = 21;
+    CGFloat labelTopView = 273;
+    self.label = [[UILabel alloc] initWithFrame:CGRectMake((screen.size.width - labelWidth)/2, labelTopView, labelWidth, labelHeight)];
+    self.label.text = @"Label";
+    self.label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.label];
+    
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"Button" forState:UIControlStateNormal];
+    
+    CGFloat buttonWidth = 46;
+    CGFloat buttonHeight = 30;
+    CGFloat buttonTopView = 374;
+    button.frame = CGRectMake((screen.size.width - buttonWidth)/2, buttonTopView, buttonWidth, buttonHeight);
+    [button addTarget:self action:@selector(onclick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+-(void)onclick:(id)sender {
+    NSInteger row1 = [self.pickerView selectedRowInComponent:0];
+    NSInteger row2 = [self.pickerView selectedRowInComponent:1];
+    NSString *selected1 = [self.pickerProvincesData objectAtIndex:row1];
+    NSString *selected2 = [self.pickerCitiesData objectAtIndex:row2];
+    
+    NSString *title = [[NSString alloc] initWithFormat:@"%@,%@市", selected1, selected2];
+    self.label.text = title;
 }
 
 
