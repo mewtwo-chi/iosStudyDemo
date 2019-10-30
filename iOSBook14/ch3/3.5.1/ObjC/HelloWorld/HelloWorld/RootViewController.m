d39f5d86745492ad2bf4cc187c6219003313ae64
mewtwo <ffchixiang@gmail.com>
2019-10-30 20:28:25 +0800

    示例

---
+++ iOSBook14/ch3/3.5.1/ObjC/HelloWorld/HelloWorld/RootViewController.m
@@ -0,0 +1,67 @@
+//
+//  RootViewController.m
+//  HelloWorld
+//
+//  Created by 关东升 on 15/12/9.
+//  本书网站：http://www.51work6.com
+//  智捷课堂在线课堂：http://www.zhijieketang.com/
+//  智捷课堂微信公共号：zhijieketang
+//  作者微博：@tony_关东升
+//  作者微信：tony关东升
+//  QQ：569418560 邮箱：eorient@sina.com
+//  QQ交流群：162030268
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIView *viewA = [[UIView alloc] init];
+    viewA.backgroundColor = [UIColor grayColor];
+    viewA.frame = CGRectMake(0,0,300,400);
+    [self.view addSubview:viewA];
+    
+    UIView *viewB = [[UIView alloc] init];
+    viewB.backgroundColor = [UIColor greenColor];
+    viewB.frame = CGRectMake(50, 100, 100, 200);
+    [viewA addSubview:viewB];
+        
+    
+//    CGRect screen = [[UIScreen mainScreen] bounds];
+//    CGFloat labelWidth = 90;
+//    CGFloat labelHeight = 20;
+//    CGFloat labelTopView = 200;
+//
+//    UILabel* label = [[UILabel alloc] initWithFrame:CGRectMake((screen.size.width - labelWidth)/2 , labelTopView, labelWidth, labelHeight)];
+//
+//    label.text = @"HelloWorld";
+//    //字体左右居中
+//    label.textAlignment = NSTextAlignmentCenter;
+//
+//    [self.view addSubview:label];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
